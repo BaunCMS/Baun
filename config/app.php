657f3c2211866981ca251bdfbc1a2538d2aa28e8
Gilbert Pellegrom <gilbert@pellegrom.me>
2015-03-03 16:56:52 +0000

    Add some inline docs to config/app.php

--- config/app.php
+++ config/app.php
@@ -2,12 +2,18 @@
 
 return [
 
+	// Path to the content folder
 	'content_path' => BASE_PATH . 'content/',
+	// The file extension to use for content files
 	'content_extension' => '.md',
 
+	// Path to the templates folder
 	'templates_path' => BASE_PATH . 'public/templates/',
+	// The currently active template
 	'template' => 'baun',
 
+	// Map the providers. Don't touch this
+	// unless you know what you are doing
 	'providers' => [
 		'contentParser' => 'Baun\Providers\ContentParser',
 		'router'        => 'Baun\Providers\Router',
