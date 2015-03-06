1bf20ef41475419525a693bac95f4ee0d767f145
Gilbert Pellegrom <gilbert@pellegrom.me>
2015-03-06 12:20:55 +0000

    Better error messages

--- public/index.php
+++ public/index.php
@@ -2,7 +2,11 @@
 
 define('BASE_PATH', dirname(__DIR__) . '/');
 
-require BASE_PATH . 'vendor/autoload.php';
+if (file_exists(BASE_PATH . 'vendor/autoload.php')) {
+	require BASE_PATH . 'vendor/autoload.php';
 
-$baun = new Baun\Baun();
-$baun->run();
+	$baun = new Baun\Baun();
+	$baun->run();
+} else {
+	die('Missing vendor/autoload.php. Run <code>composer install</code>.');
+}
