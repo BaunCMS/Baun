cf3c4de5f42c32d716495a6dd7a2e5437cea10ae
Gilbert Pellegrom <gilbert@pellegrom.me>
2015-03-06 12:20:40 +0000

    Add new config options

--- config/app.php
+++ config/app.php
@@ -2,6 +2,9 @@
 
 return [
 
+	// Enable debug mode
+	'debug' => false,
+
 	// Path to the content folder
 	'content_path' => BASE_PATH . 'content/',
 	// The file extension to use for content files
@@ -11,6 +14,16 @@ return [
 	'themes_path' => BASE_PATH . 'public/themes/',
 	// The currently active theme
 	'theme' => 'baun',
+
+	// The name of the folder where your posts are stored
+	// (can be number prefixed but don't include that here)
+	'blog_folder' => 'blog',
+	// For blog pagination
+	'posts_per_page' => 10,
+	// The length of excerpts in words
+	'excerpt_words' => 30,
+	// The date format
+	'date_format' => 'jS F Y',
 
 	// Map the providers. Don't touch this
 	// unless you know what you are doing
