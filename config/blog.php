d29a56e2a291b0330a32b87b8bcdb4fc9692d5c1
Gilbert Pellegrom <gilbert@pellegrom.me>
2015-03-09 12:13:03 +0000

    Split up config files

---
+++ config/blog.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+	// The name of the folder where your posts are stored
+	// (can be number prefixed but don't include that here)
+	'blog_folder' => 'blog',
+	// For blog pagination
+	'posts_per_page' => 10,
+	// The length of excerpts in words
+	'excerpt_words' => 30,
+	// The date format
+	'date_format' => 'jS F Y',
+
+];
---
+++ config/providers.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+	// Map the providers. Don't touch this
+	// unless you know what you are doing
+	'contentParser' => 'Baun\Providers\ContentParser',
+	'events'        => 'Baun\Providers\Events',
+	'router'        => 'Baun\Providers\Router',
+	'theme'         => 'Baun\Providers\Theme',
+
+];
